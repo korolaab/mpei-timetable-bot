b9f127408e7624a647626d36e8a531e795f986b7
psylopunk <psylopunk@users.noreply.github.com>
2020-08-31 13:09:33 +0300

    add default keyboard

--- server/server.py
+++ server/server.py
@@ -1,20 +1,25 @@
 from sanic import Sanic, response
-from telebot import TeleBot, types, apihelper
+from modules import Memory, User
 import config
 
+memory = Memory()
 app = Sanic(__name__)
 bot = TeleBot(config.TELEGRAM_BOT_KEY)
 
 @app.route("/")
-def s_index(request):
+await def s_index(request):
     return response.text("OK")
 
 @app.route("/t_webhook", methods=["GET", "POST"])
-def s_twebhook(request):
+await def s_twebhook(request):
     print(request.json)
     data = request.json
     if "message" in data:
         data = data["message"]
+        user = memory.get_user_by_chat(data["chat"])
+        text = data["text"]
+        if text == "/start":
+            user.send_welcome()
     return response.text("OK")
 
 if __name__ == '__main__':
