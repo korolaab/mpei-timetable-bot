552b7a7201978d031fe0b71f60c3834e371a9327
psylopunk <psylopunk@users.noreply.github.com>
2020-09-05 17:25:30 +0300

    Revert "fix"
    This reverts commit a41e4d5f19fb4545d7ff76039357100a087092d3.

--- server/models.py
+++ server/models.py
@@ -148,9 +148,8 @@ class User:
             text=text, parse_mode="html", *args, **kwargs)
             if m:
                 self.message_id = m.message_id
-                db.users.update_one({"_id": self.db_id}, {"$set": {"message_id": m.message_id}})
-                return m
-            return False
+                db.users.update_one({"_id": self.db_id}, {"$set": {"message_id": m.message_id}}
+            return m
         except apihelper.ApiException as e:
             print("Error: [%s] (caused by edit_message)" % e)
             return False
